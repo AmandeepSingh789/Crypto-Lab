66820237b655920f2edb99e0beb260cd97d02d75
Amandeep <singh.amandeep5665@gmail.com>
2022-05-02 14:39:38 -0700

    more stuff

--- src/App.js
+++ src/App.js
@@ -1,46 +1,56 @@
-import React from 'react';
-import { Routes,Route,Link } from 'react-router-dom';
-import { Layout,Typography,Space } from 'antd';
-import './App.css';
-import {Navbar,Exchanges,Homepage,CryptoDetails,Cryptocurrencies,News} from './components';
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
+import "./App.css";
+import {
+  Navbar,
+  Exchanges,
+  Homepage,
+  CryptoDetails,
+  Cryptocurrencies,
+  News,
+} from "./components";
 
 const App = () => {
   return (
-    <div className='app'>
-        <div className='navbar'>
-            <Navbar />
-        </div>
-        <div className='main'>
-            <Layout>
-                <div className='routes'>
-                    <Routes>
-                    <Route exact path="/" element={
-                    <Homepage/>
-                    }/>
-                    <Route exact path="/exchanges" element={
-                    <Exchanges/>
-                    }/>
-                    <Route exact path="/cryptocurrencies" element={
-                    <Cryptocurrencies/>
-                    }/>
-                    <Route exact path="/crypto/:coinId" element={
-                    <CryptoDetails/>
-                    }/>
-                    <Route exact path="/news" element={
-                    <News/>
-                    }/>
-                    </Routes>
-                </div>
-            </Layout>
-        </div>
-        <div className='footer'>
-            <Typography.Title>
-                Crypto Lab <br/>
-                All Rights Reserved
-            </Typography.Title>
-        </div>
-    </div>
-  )
-}
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route exact path="/" element={<Homepage />} />
+              <Route exact path="/exchanges" element={<Exchanges />} />
+              <Route
+                exact
+                path="/cryptocurrencies"
+                element={<Cryptocurrencies />}
+              />
+              <Route exact path="/crypto/:coinId" element={<CryptoDetails />} />
+              <Route exact path="/news" element={<News />} />
+            </Routes>
+          </div>
+        </Layout>
 
-export default App
+        <div className="footer">
+          <Typography.Title
+            level={5}
+            style={{ color: "white", textAlign: "center" }}
+          >
+            Crypto Lab <br />
+            All Rights Reserved
+          </Typography.Title>
+          <Space>
+            <Link to="/">Home</Link>
+            <Link to="/exchanges">Exchanges</Link>
+            <Link to="/news">News</Link>
+          </Space>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+
 import App from './App';
 import 'antd/dist/antd.css';
+import store from './app/store';
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -10,7 +13,9 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
         <Router>
+            <Provider store = {store}>
             <App />
+            </Provider>
         </Router>
     
   </React.StrictMode>
