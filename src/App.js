c90c67ce0061a07bfef6c10aa27f87480025928f
Amandeep <singh.amandeep5665@gmail.com>
2022-05-02 14:15:10 -0700

    more stuff added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Switch,Route,Link } from 'react-router-dom';
+import { Routes,Route,Link } from 'react-router-dom';
 import { Layout,Typography,Space } from 'antd';
 import './App.css';
-import {Navbar} from './components';
+import {Navbar,Exchanges,Homepage,CryptoDetails,Cryptocurrencies,News} from './components';
 
 const App = () => {
   return (
@@ -11,10 +11,33 @@ const App = () => {
             <Navbar />
         </div>
         <div className='main'>
-
+            <Layout>
+                <div className='routes'>
+                    <Routes>
+                    <Route exact path="/" element={
+                    <Homepage/>
+                    }/>
+                    <Route exact path="/exchanges" element={
+                    <Exchanges/>
+                    }/>
+                    <Route exact path="/cryptocurrencies" element={
+                    <Cryptocurrencies/>
+                    }/>
+                    <Route exact path="/crypto/:coinId" element={
+                    <CryptoDetails/>
+                    }/>
+                    <Route exact path="/news" element={
+                    <News/>
+                    }/>
+                    </Routes>
+                </div>
+            </Layout>
         </div>
         <div className='footer'>
-
+            <Typography.Title>
+                Crypto Lab <br/>
+                All Rights Reserved
+            </Typography.Title>
         </div>
     </div>
   )
