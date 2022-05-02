4578f78e7d9b838b830354210bdd615d45c2a83f
Amandeep <singh.amandeep5665@gmail.com>
2022-05-02 15:58:20 -0700

    added search crypto functionality

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState,useEffect} from 'react'
 import millify from 'millify';
 import thousandify from 'thousandify';
 import { Link } from 'react-router-dom';
@@ -8,17 +8,31 @@ import { useGetCryptosQuery } from '../services/cryptoApi';
 
 
 
-const Cryptocurrencies = (simplified) => {
+const Cryptocurrencies = ({simplified}) => {
 
   const count = simplified ? 10 : 100;
   const{data:cryptosList,isFetching} = useGetCryptosQuery(count);
-  const [cryptos, setCryptos] = useState(cryptosList?.data?.coins);
+  const [cryptos, setCryptos] = useState([]);
+  const [searchTerm,setSearchTerm] = useState('');
+
+  useEffect(()=> {
+      const filteredData = cryptosList?.data?.coins.filter((coin)=> coin.name.toLowerCase().includes(searchTerm.toLowerCase()));
+      setCryptos(filteredData);
+    },[cryptosList,searchTerm]);
+
   
   console.log(cryptos);
 
   if(isFetching) return 'Loading ...';
   return (
     <>
+    {!simplified && (
+        <div className='search-crypto'>
+        <Input placeholder='Search Cryptocurrency' onChange={(e) => setSearchTerm(e.target.value)} />
+  
+      </div>
+    )}
+    
       <Row gutter={[32,32]} className="crypto-card-container">
         {cryptos?.map((currency) => (
           <Col xs={24} sm={12} lg={6} className="crypto-card" key = {currency.id}>
--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -33,7 +33,7 @@ const Homepage = () => {
         <Title level={2} className = "home-title">Top 10 Cryptocurrencies in the world</Title>
         <Title level={3} className = "show-more"><Link to="/cryptocurrencies">Show More</Link></Title>
     </div>
-    <Cryptocurrencies simplified={true}/>
+    <Cryptocurrencies simplified />
     <div className='home-heading-container'>
         <Title level={2} className = "home-title">Show Latest News</Title>
         <Title level={3} className = "show-more"><Link to="/news">Show More</Link></Title>
